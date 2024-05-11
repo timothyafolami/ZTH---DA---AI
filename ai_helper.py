cbe2cdf55db3679e41b05015fa6d21e943a7c353
Afolami Timothy Olawale <timmyafolami8469@gmail.com>
2024-05-11 04:13:27 +0100

    upppp

--- ai_helper.py
+++ ai_helper.py
@@ -48,3 +48,35 @@ def cleaner_rec_ai(input: str) -> str:
     chain = prompt | chat
     topic_dis = chain.invoke({"text": {input}})
     return topic_dis.content
+
+# chat set up
+class DataScienceConsultant:
+    def __init__(self, temperature=0.5, model_name="llama3-8b-8192"):
+        self.chat = ChatGroq(temperature=temperature, model_name=model_name)
+        self.template = """You are a Data Science Consultant. You have over 20 years of experience in the field. 
+        You are currently working with a client to create synthetic data for their product. 
+        You don't know anything about the product yet, which is why you want to ask the client some questions to understand the data requirements.
+        This is the workflow you have been following:
+            1. Converse with the client to understand the data requirements.
+            2. Ask questions about the product the client is working on.
+            3. Ask for possible columns and the data types. 
+            4. Ask for the number of rows and the distribution of the data.
+            5. Create a Python script that the client can work with to generate the data.
+            6. Review the generated code with the client requirements.
+
+        Return the code to the client for review.
+
+        Current conversation:
+        {history}
+        Human: {input}
+        AI Assistant:"""
+        self.prompt = PromptTemplate(input_variables=["history", "input"], template=self.template)
+        self.conversation = ConversationChain(
+            prompt=self.prompt,
+            llm=self.chat,
+            verbose=True,
+            memory=ConversationBufferWindowMemory(k=10, ai_prefix="AI Assistant"),
+        )
+
+    def predict(self, input_text):
+        return self.conversation.predict(input=input_text)
--- pages/chat.py
+++ pages/chat.py
@@ -1,11 +1,71 @@
 import streamlit as st
 from streamlit_chat import message
+from langchain.callbacks.base import BaseCallbackHandler
+from langchain.chains import ConversationChain
+from ai_helper import DataScienceConsultant
 
 
-st.title("Chat Page with you PDF file")
+st.set_page_config(page_title='🤖 Data Generator Assistant', layout='centered', page_icon='🤖')
+st.title("🤖 Chat with AI")
 
-
+# initial message
 INIT_MESSAGE = {"role": "assistant",
-                "content": "Hello! I'm Muchacha Orderbot. How may I help you?"}
+                "content": "Hello! I am a Data Science Consultant. I will help you create the right data for your product. "}
 
 
+def init_conversationchain() -> ConversationChain:
+    chat_executor = DataScienceConsultant()
+
+    # Store LLM generated responses
+    if "messages" not in st.session_state:
+        st.session_state.messages = [INIT_MESSAGE]
+
+    return chat_executor
+
+def generate_response(conversation: ConversationChain, input_text: str) -> str:
+    try:
+        response = conversation.predict(input_text)
+    except ValueError as e:
+        print("################",str(e))
+        response = str(e)
+        if not response.startswith("Could not parse LLM output: `"):
+            response = "There were some error in answering this question. "
+        else:
+            response = response.removeprefix("Could not parse LLM output: `").removesuffix("`")
+    return response
+
+
+# Re-initialize the chat
+def new_chat() -> None:
+    st.session_state["messages"] = [INIT_MESSAGE]
+    st.session_state["langchain_messages"] = []
+    conv_chain = init_conversationchain()
+
+
+# Add a button to start a new chat
+st.sidebar.button("New Chat", on_click=new_chat, type='primary')
+
+
+# Initialize the conversation chain
+conversation = init_conversationchain()
+
+# Display chat messages
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Get user input
+user_input = st.chat_input(placeholder="Your message ....", key="input")
+
+# display user input
+if user_input:
+    st.session_state.messages.append({"role": "user", "content": user_input})
+    user_message = st.chat_message("user")
+    user_message.write(user_input)
+
+# Generate response
+if st.session_state.messages[-1]["role"] != "assistant":
+    response = generate_response(conversation, user_input)
+    st.session_state.messages.append({"role": "assistant", "content": response})
+    assistant_message = st.chat_message("assistant")
+    assistant_message.write(response)
